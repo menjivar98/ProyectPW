43b1b722f7e801e6e8167e09ba54afda63543763
RichardF12 <37163455+RichardF12@users.noreply.github.com>
2018-11-30 14:48:50 -0600

    Modifying css

--- src/index.js
+++ src/index.js
@@ -105,10 +105,11 @@ app.listen(app.get('port'), () => {
   console.log('Server on port', app.get('port'));
 });
 
-//Heroku config
+/*Heroku config
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
 const DB_URI = process.env.DB_URI
 const PORT = process.env.PORT
+*/
